77a51c1463b952fb68e4007ce4f87196968eb762
Duc Do <duc.fet01@gmail.com>
2017-03-05 19:18:20 +0200

    Add graphQL

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     name: "whimEat",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/GraphQLSwift/GraphQL.git", majorVersion: 0, minor: 2)
     ],
     exclude: [
         "Config",
